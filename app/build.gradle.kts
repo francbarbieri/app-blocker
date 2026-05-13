e331a75db746ad61798de947daafcb531658b8b5
francbarbieri <fbarbierisosa@gmail.com>
2026-05-13 15:58:54 -0300

    Add test deps: coroutines-test, turbine, room-testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,5 +74,8 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("app.cash.turbine:turbine:1.1.0")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
 }
